29c69486adced5939e1d8c43e0d7395b084b8bfb
oilpaintingkv <oilpaintingkv@gmail.com>
2023-10-31 11:19:02 +0900

    react basic 2 (state)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,34 +2,15 @@ import React from 'react';
 // import './App.css';
 
 import MyHeader from './MyHeader';
+import Counter from './Counter';
 
 function App() {
-  let name = 'oilpaintingKV';
-
-  const style = {
-    // 인라인 방식 style 지정 -> style 객체 만들기
-    App: {
-      backgroundColor: 'black',
-    },
-    h2: {
-      color: 'white',
-    },
-    type_bold: {
-      color: 'white',
-      fontWeight: 'bold',
-    },
-  };
-
   const number = 5;
 
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>hi react!</h2>
-      <span style={style.type_bold}>
-        React.js
-        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-      </span>
+      <Counter />
     </div>
   );
 }
