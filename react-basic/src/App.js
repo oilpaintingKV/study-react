7fb739b502f8a81244527f12c0986123592db2e9
oilpaintingkv <oilpaintingkv@gmail.com>
2023-10-31 14:04:00 +0900

    react basic 3 (props)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -3,15 +3,25 @@ import React from 'react';
 
 import MyHeader from './MyHeader';
 import Counter from './Counter';
+import Container from './Container';
 
 function App() {
   const number = 5;
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+  };
 
   return (
-    <div>
-      <MyHeader />
-      <Counter />
-    </div>
+    <Container>
+      <div>
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
