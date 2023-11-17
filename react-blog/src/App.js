72106432c1954c2f414d2d217117b094b3a7796e
oilpaintingkv <oilpaintingkv@gmail.com>
2023-11-17 16:13:02 +0900

    state

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let post = 'Gangnam kimchi Restaurant';
+  let [title, setTitle] = useState([
+    "a recommendation for women's shoes",
+    'Recommend a good restaurant',
+    'Crime City 3 review',
+  ]);
+
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h1 style={{ fontSize: '20px', color: 'gray' }}>BLOG</h1>
+        <h1 style={{ fontSize: '20px', color: 'gray' }}>ReactBlog</h1>
       </div>
-      <h2>{post}</h2>
+      <div className='list'>
+        <h2 className='tit'>{title[0]}</h2>
+        <span className='date'>11/17</span>
+      </div>
+      <div className='list'>
+        <h2 className='tit'>{title[1]}</h2>
+        <span className='date'>11/17</span>
+      </div>
+      <div className='list'>
+        <h2 className='tit'>{title[2]}</h2>
+        <span className='date'>11/17</span>
+      </div>
     </div>
   );
 }
